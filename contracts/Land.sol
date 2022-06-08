174d9162abf968d4056b3d58cd799a4413a8a627
Abril Zucchi <abrilzucchi@MacBook-Air-de-Abril.local>
2022-06-08 15:51:09 -0300

    feat: implementing interfaces ERC721

---
+++ contracts/Land.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.7;
+
+import '../contracts/IERC721Base.sol';
+import '../contracts/IERC721Enumerable.sol'; 
+import "@openzeppelin/contracts/access/Ownable.sol";
+
