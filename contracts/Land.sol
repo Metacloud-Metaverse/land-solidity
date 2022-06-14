ae654da0c833c515db0903f5d19c6b4902ae44a6
Abril Zucchi <abrilzucchi@Air-de-Abril.fibertel.com.ar>
2022-06-14 13:01:43 -0300

    feat: land contract refactor and add requirements of buy and mint land

--- contracts/Land.sol
+++ contracts/Land.sol
@@ -1,7 +1,97 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.7;
+pragma solidity 0.8.14;
 
-import '../contracts/IERC721Base.sol';
-import '../contracts/IERC721Enumerable.sol'; 
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 
+contract Land is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
+    using Counters for Counters.Counter;
+    IERC721 public cloud;
+
+    Counters.Counter private _tokenIdCounter;
+
+    constructor() ERC721("LAND", "LND") {
+
+    }
+
+
+
+    // Mint with almost no requirements, reserved for companies
+    function safeMint(address to, string memory uri) public onlyOwner {
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+
+
+
+    //Mint with ERC-721
+    function safeMintWithcCloud(address to, string memory uri, uint _id) public  {
+        //requirement that the sender have the id passed as a parameter
+        require(cloud.ownerOf(_id) == msg.sender, "you must be owner of id");
+
+        //transfer the cloud token to where it is required, burn company wallet etc.
+        cloud.transferFrom(msg.sender, address(this), _id);
+        
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+
+
+
+    function setCloud(IERC721 _cloud) public {
+      cloud = _cloud;
+    }
+
+
+    // The following functions are overrides required by Solidity.
+
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+
+
+
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+
+
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+
+
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
