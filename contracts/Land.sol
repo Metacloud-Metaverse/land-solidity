7bf3382abaeeb196a78cfeb4241addf886afae97
Abril Zucchi <abrilzucchi@Air-de-Abril.fibertel.com.ar>
2022-06-14 23:33:46 -0300

    feat: refactor land NFT, add function buy & mint

--- contracts/Land.sol
+++ contracts/Land.sol
@@ -2,26 +2,44 @@
 pragma solidity 0.8.14;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract Land is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Ownable {
+contract LAND is
+    ERC721,
+    ERC721Enumerable,
+    ERC721URIStorage,
+    ERC721Burnable,
+    Ownable
+{
     using Counters for Counters.Counter;
-    IERC721 public cloud;
+
+    IERC20 public cloud;
+    address public house;
+
+    uint256 public cloudPrice;
+    uint256 public bnbPrice;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("LAND", "LND") {
-
+    constructor(
+        IERC20 _cloud,
+        address _house,
+        uint256 _cloudPrice,
+        uint256 _bnbPrice
+    ) ERC721("Land", "LAND") {
+        cloud = _cloud;
+        house = _house;
+        cloudPrice = _cloudPrice;
+        bnbPrice = _bnbPrice;
     }
 
-
-
-    // Mint with almost no requirements, reserved for companies
+    //Mint reserved for the owner
     function safeMint(address to, string memory uri) public onlyOwner {
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
@@ -29,50 +47,66 @@ contract Land is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
         _setTokenURI(tokenId, uri);
     }
 
+    //  Mint with ERC-20 (CLOUD TOKEN)
+    function safeMintWithCloud(address to, string memory uri) public {
+        // Requirement: the user have the token CLOUD
+        require(
+            cloud.balanceOf(msg.sender) >= cloudPrice,
+            "You dont have balance"
+        );
 
+        // Transfer the cloud token to where it is required. For example: project address 'Metacloud' so that the Land ID can be automatically generated for the user who bought it.
+        require(cloud.transferFrom(msg.sender, house, cloudPrice));
 
-
-    //Mint with ERC-721
-    function safeMintWithCloud(address to, string memory uri, uint _id) public  {
-        //requirement that the sender have the id passed as a parameter
-        require(cloud.ownerOf(_id) == msg.sender, "you must be owner of id");
-
-        //transfer the cloud token to where it is required, burn company wallet etc.
-        cloud.transferFrom(msg.sender, address(this), _id);
-        
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
     }
 
+    // Mint with BNB
+    function safeMintWithBNB(address to, string memory uri) public payable {
+        require(msg.value >= bnbPrice, "You dont have balance");
+        payable(address(house)).transfer(msg.value);
 
-
-
-    function setCloud(IERC721 _cloud) public {
-      cloud = _cloud;
+        uint256 tokenId = _tokenIdCounter.current();
+        _tokenIdCounter.increment();
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
     }
 
+    function setCloud(IERC20 _cloud) public {
+        cloud = _cloud;
+    }
+
+    function sethouse(address _house) public {
+        house = _house;
+    }
+
+    function setCloudPrice(uint256 _cloudPrice) public {
+        cloudPrice = _cloudPrice;
+    }
+
+    function setBnbPrice(uint256 _bnbPrice) public {
+        bnbPrice = _bnbPrice;
+    }
 
     // The following functions are overrides required by Solidity.
 
-    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-        internal
-        override(ERC721, ERC721Enumerable)
-    {
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
         super._beforeTokenTransfer(from, to, tokenId);
     }
 
-
-
-
-
-    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    function _burn(uint256 tokenId)
+        internal
+        override(ERC721, ERC721URIStorage)
+    {
         super._burn(tokenId);
     }
-
-
-
 
     function tokenURI(uint256 tokenId)
         public
@@ -82,9 +116,6 @@ contract Land is ERC721, ERC721Enumerable, ERC721URIStorage, ERC721Burnable, Own
     {
         return super.tokenURI(tokenId);
     }
-
-
-
 
     function supportsInterface(bytes4 interfaceId)
         public
