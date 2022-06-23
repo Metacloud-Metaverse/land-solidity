0ceff4bac1b8ab89fe303ed8d9be703f76c3f948
Franco <franco.nisi@my.com>
2022-06-23 11:58:51 -0300

    fix: Land minting fix and maxSupply

--- contracts/Land.sol
+++ contracts/Land.sol
@@ -4,96 +4,85 @@ pragma solidity 0.8.15;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
-import "../interfaces/IMarketplace.sol"; 
+import "./interfaces/IMarketplace.sol"; 
 
 
-contract Land is ERC721Enumerable, Ownable { 
-  using Counters for Counters.Counter;
-  Counters.Counter private _tokenIdCounter;
+contract Land is ERC721, ERC721Enumerable, Ownable { 
+    using Address for address;
+    using Counters for Counters.Counter;
 
+    Counters.Counter private _tokenIdCounter;
 
+    struct LandMetadata{
+        uint256 coordinatesX;
+        uint256 coordinatesY;
+    }
 
-  
-  struct LandMetadata{
-    uint256 coordinatesX;
-    uint256 coordinatesY;
-  }
+    mapping(uint256 => LandMetadata) public landIndexToMetadata;
+    IMarketplace public marketplace;
+    uint256 public immutable maxSupply = 10000;
 
+    constructor() ERC721("Land","LAND") {}
 
-
-
-  mapping(uint256 => LandMetadata) public index;
-  IMarketplace public marketplace;
-  uint256 public maxSupply;
-
-
-
-
-  constructor() ERC721("Land","LAND"){
-  }
-
-
-
-
-  function safeMint(
-    address to, 
-    uint256 _coordinatesX,
-    uint256 _coordinatesY
+    function safeMint(
+        address _to, 
+        uint256 _coordinatesX,
+        uint256 _coordinatesY
     ) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();        
-        index[tokenId] = LandMetadata({
+        uint256 tokenId = _tokenIdCounter.current();
+        require(tokenId < maxSupply, "ERC721: minting would exceed total supply");
+        _tokenIdCounter.increment();
+        // Save Land metadata
+        landIndexToMetadata[tokenId] = LandMetadata({
             coordinatesX: _coordinatesX,
             coordinatesY: _coordinatesY
         });
+        _safeMint(_to, tokenId);
+    }
 
-        _safeMint(to, tokenId);
-  }
+    function setMarketAddress(address _marketplace) public onlyOwner {
+        require(_marketplace != address(0), "Marketplace address cannot be 0");
+        require(_marketplace.isContract(), "Marketplace address must be a deployed contract");
+        marketplace = IMarketplace(_marketplace);
+    }
 
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokenId
+    ) public override(ERC721, IERC721){
+        require(address(marketplace) != address(0),"Marketplace address is not setted");
+        require(!marketplace.assetIdToOrderOpen(tokenId), "Land has an open order in Marketplace, cancel before transfer");
+        require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
 
+        super._transfer(from, to, tokenId);
+    }
 
+    function safeTransferFrom(
+        address from,
+        address to,
+        uint256 tokenId,
+        bytes memory _data
+    ) public virtual override(ERC721, IERC721) {
+        transferFrom(from, to, tokenId);
+    }
 
-  function setMarketAddress(address _marketplace) public onlyOwner{
-    marketplace = IMarketplace(_marketplace);
-  }
+    // The following functions are overrides required by Solidity.
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId)
+        internal
+        override(ERC721, ERC721Enumerable)
+    {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
 
-
-
-
-  function transferFrom(
-    address from,
-    address to,
-    uint256 tokenId
-  ) public override(ERC721, IERC721){
-    require(address(marketplace) != address(0),"Address of marketplace is not set");
-    require(!marketplace.assetIdToOrderOpen(tokenId), "This token is in marketplace");
-    require(_isApprovedOrOwner(_msgSender(), tokenId), "ERC721: transfer caller is not owner nor approved");
-
-   super._transfer(from, to, tokenId);
-
-  }
-
-
-
-
-  // The following functions are overrides required by Solidity.
-  function _beforeTokenTransfer(address from, address to, uint256 tokenId)
-      internal
-      override(ERC721Enumerable)
-  {
-      super._beforeTokenTransfer(from, to, tokenId);
-  }
-
-
-
-
-  function supportsInterface(bytes4 interfaceId)
-      public
-      view
-      override(ERC721Enumerable)
-      returns (bool)
-  {
-      return super.supportsInterface(interfaceId);
-  }
-
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,12 @@
 require("@nomiclabs/hardhat-waffle");
+require('hardhat-deploy');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.15",
+    solidity: "0.8.15",
+    namedAccounts: {
+        deployer: {
+                default: 0, // take the first account as deployer by default
+                1: 0 // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+        },
+    },
 };
